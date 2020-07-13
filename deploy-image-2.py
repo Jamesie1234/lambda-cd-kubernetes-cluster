be985676d4de2577294eb53b4173a356373836bb
Adesanluj <james.adesanlu@informed.com>
2020-07-13 10:17:38 +0100

    remove temporary keys

---
+++ deploy-image-2.py
@@ -0,0 +1,21 @@
+import boto3
+import os
+import logging
+import csv
+
+
+
+
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
+#
+#
+# def lambda_handler(event, context):
+#     try:
+#         env = event[os.environ[]'env', dev)]
+#         print(env)
+#     except Exception as e:
+#         return {
+#           'statusCode':400,
+#           'body': str(e)
+#         }
